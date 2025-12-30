93cb2c32865b82bb33a3a477b073ec66eda4621f
konsuki <konsuki2@icloud.com>
2025-12-30 16:37:24 +0900

    アプリ名変更

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://konsuki.github.io/my-gh-pages-test', // あなたのサイトのURL
+  siteUrl: 'https://konsuki.github.io/replab', // あなたのサイトのURL
   generateRobotsTxt: true, // robots.txtも自動生成する
   outDir: 'out', // create-next-appのoutput: 'export'先であるoutフォルダに出力する
 }
